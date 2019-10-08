793ffaa6e7ab2cadd737b28332fe1988000a8a80
climba03003 <climba03003@gmail.com>
2019-10-08 17:54:30 +0800

    Fix: Missing files in Nexmo

--- lib/nexmo.d.ts
+++ lib/nexmo.d.ts
@@ -1,8 +1,10 @@
 import Credentials, { INexmoCredential } from './credential';
 import NexmoCalls from './calls';
+import NexmoFiles from './files';
 export declare class Nexmo {
     credential: Credentials;
     calls: NexmoCalls;
+    files: NexmoFiles;
     constructor(credential: Credentials | INexmoCredential);
 }
 export default Nexmo;
--- src/nexmo.ts
+++ src/nexmo.ts
@@ -1,14 +1,17 @@
 import Credentials, { INexmoCredential } from './credential';
 import NexmoCalls from './calls';
+import NexmoFiles from './files';
 
 export class Nexmo {
   credential: Credentials;
   calls: NexmoCalls;
+  files: NexmoFiles;
 
   constructor(credential: Credentials | INexmoCredential) {
     this.credential = Credentials.parse(credential);
 
     this.calls = new NexmoCalls(this.credential);
+    this.files = new NexmoFiles(this.credential);
   }
 }
 
